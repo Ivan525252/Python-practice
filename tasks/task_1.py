56a55f739011defd7bbeec2281df96d7e5701198
Ivan chepur <vanyachepur2013@gmail.com>
2024-12-14 15:36:19 +0300

    Новые ошибки исправлены

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -1,13 +1,13 @@
 age = int(input("Напишите свой age  "))
 is_admin = input("Проверка на админа, true or false  ")
 
-if 20 <= age and is_admin == "false" :
+if 20 <= age and is_admin == "false":
     print("Доступ разрешён") 
 
-if age <= 19 :
+if 19 >= age:
     print("Доступ запрещён")
 
 if 20 < age and is_admin == "true":
-    print( "Доступ разрешён. Добро пожаловать администратор!")
+    print("Доступ разрешён. Добро пожаловать администратор!")
 
 
