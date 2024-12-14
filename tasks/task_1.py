f108290b899ec55efd840cb18fdb4631e39f4bc5
Ivan chepur <vanyachepur2013@gmail.com>
2024-12-14 16:25:24 +0300

    Рефакторинг if

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -1,13 +1,11 @@
 age = int(input("Напишите свой age  "))
-is_admin = input("Проверка на админа, true or false  ")
-
-if 20 <= age and is_admin == "false":
-    print("Доступ разрешён") 
-
-if 19 >= age:
-    print("Доступ запрещён")
-
+is_admin = input("Проверка на админа, true or false")
+ 
 if 20 <= age and is_admin == "true":
     print("Доступ разрешён. Добро пожаловать администратор!")
+elif 20 <= age and is_admin == "false":
+    print("Доступ разрешён")
+else :
+    print("Доступ запрещён")
 
 
