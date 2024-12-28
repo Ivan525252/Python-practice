eca715139c38182f1b596c1b53b33d60976e658d
Ivan chepur <vanyachepur2013@gmail.com>
2024-12-28 15:39:49 +0300

    Ошыбки в task_3_optional_calculator.py исправлены

--- tasks/task_3_optional_calculator.py
+++ tasks/task_3_optional_calculator.py
@@ -1,19 +1,19 @@
 first_number = float(input("Введите первое число: "))
-sekond_number = float(input("Введите второе число: "))
+second_number = float(input("Введите второе число: "))
 operator = (input("Введите оператор (+, -, *, /):"))
 
-ответ_1 = first_number + sekond_number
-if operator == "+" and first_number + sekond_number:
-    print("Результат:{ответ_1}")
+ответ_1 = first_number + second_number
+if operator == "+" :
+    print(f"Результат:{ответ_1}")
 
-ответ_2 = first_number - sekond_number
-if operator == "-" and first_number - sekond_number:
-    print("Результат:{ответ_2}")
+ответ_2 = first_number - second_number
+if operator == "-" :
+    print(f"Результат:{ответ_2}")
 
-ответ_3 = first_number * sekond_number
-if operator == "*" and first_number * sekond_number:
-    print("Результат:{ответ_3}")
+ответ_3 = first_number * second_number
+if operator == "*" :
+    print(f"Результат:{ответ_3}")
 
-ответ_4 = first_number / sekond_number
-if operator == "/" and first_number / sekond_number:
-    print("Результат:{ответ_4}")
+ответ_4 = first_number / second_number
+if operator == "/" :
+    print(f"Результат:{ответ_4}")
