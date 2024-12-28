8aa7043771a5a9d8f9f7fba1c6ad99082d24bc62
Ivan chepur <vanyachepur2013@gmail.com>
2024-12-28 16:07:34 +0300

    Рефакторинг

--- tasks/task_3_optional_calculator.py
+++ tasks/task_3_optional_calculator.py
@@ -2,18 +2,17 @@ first_number = float(input("Введите первое число: "))
 second_number = float(input("Введите второе число: "))
 operator = (input("Введите оператор (+, -, *, /):"))
 
-ответ_1 = first_number + second_number
-if operator == "+" :
-    print(f"Результат:{ответ_1}")
-
-ответ_2 = first_number - second_number
-if operator == "-" :
-    print(f"Результат:{ответ_2}")
-
-ответ_3 = first_number * second_number
-if operator == "*" :
-    print(f"Результат:{ответ_3}")
-
-ответ_4 = first_number / second_number
-if operator == "/" :
-    print(f"Результат:{ответ_4}")
+if operator == "+":
+    result = first_number + second_number
+    print(f"Результат:{result}")
+elif operator == "-":
+    result = first_number - second_number
+    print(f"Результат:{result}")
+elif operator == "*":
+    result = first_number * second_number
+    print(f"Результат:{result}")
+elif operator == "/":
+    result = first_number / second_number
+    print(f"Результат:{result}")
+else:
+    print("Ошыбка, пожалуста ведите оператор")
