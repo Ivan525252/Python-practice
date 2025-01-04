2e082bb8745dba49048ee80ca4fb48bf7d4a3671
Ivan chepur <vanyachepur2013@gmail.com>
2025-01-04 12:41:26 +0300

    Я зделал програму exchange_rate

---
+++ tasks/task_4_exchange.py
@@ -0,0 +1,6 @@
+first_number = float(input("Введите количество долларов: "))
+second_number = float(input("Введите текущий курс доллара: "))
+
+result = first_number * second_number
+if first_number * second_number :
+    print(f"Сумма в гривнах: {result} ")
