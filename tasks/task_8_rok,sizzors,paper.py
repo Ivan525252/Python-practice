1927ec476251b7bddd35088015e48e2b9919df39
Ivan chepur <vanyachepur2013@gmail.com>
2025-03-01 15:45:34 +0300

    Рефакторинг проверки и if

--- tasks/task_8_rok,sizzors,paper.py
+++ tasks/task_8_rok,sizzors,paper.py
@@ -1,15 +1,20 @@
 import random
-sign = input("Выбирете: камень, ножницы или бумага  ")
+sign = input("Выбирете: камень, ножницы или бумага:")
 random_sign = random.randint( 1, 3 )
+
 if random_sign == 1:
     random_sign = "камень"
 elif random_sign == 2:
     random_sign = "ножницы"
 elif random_sign == 3:
     random_sign = "бумага"
-print(f"Комьютер выбрал: {random_sign}")
+
 print(f"Я выбрал: {sign}")
-if sign == random_sign:
+print(f"Комьютер выбрал: {random_sign}")
+
+if not (sign in {"камень", "ножницы", "бумага"}):
+    print("Ошыбка, пожалуста впишытие знак")
+elif sign == random_sign:
     print("Ничья!")
 elif ((sign == "камень" and random_sign == "бумага") 
     or (sign == "ножницы" and random_sign == "камень") 
