2958cb17b3024cace265351210c3f888b6264841
Ivan chepur <vanyachepur2013@gmail.com>
2025-03-08 15:44:03 +0300

    Пробелы были исправлины

--- tasks/task_9_namelength.py
+++ tasks/task_9_namelength.py
@@ -1,6 +1,7 @@
 name = input("Введи имя:")
 len_name = len(name)
-if len_name > 6 :
+
+if len_name > 6:
     print("У тебя длинное имя")
-elif len_name <= 6 :
+elif len_name <= 6:
     print("У тебя короткое имя")
