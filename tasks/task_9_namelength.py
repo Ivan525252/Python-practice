583999f58ca11ba1a2f942f62e1e6e3059215953
Ivan chepur <vanyachepur2013@gmail.com>
2025-03-08 15:33:39 +0300

    Я исправил програму namelength

--- tasks/task_9_namelength.py
+++ tasks/task_9_namelength.py
@@ -1,7 +1,6 @@
-name = int(input("Введи имя:"))
-len(name)
-if name > 6 :
+name = input("Введи имя:")
+len_name = len(name)
+if len_name > 6 :
     print("У тебя длинное имя")
-elif name < 6 :
+elif len_name <= 6 :
     print("У тебя короткое имя")
-                    
